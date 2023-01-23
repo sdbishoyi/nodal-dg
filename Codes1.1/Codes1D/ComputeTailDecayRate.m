4dfa93eb800c5e49d22112e614690e9f47cf1b89
Som <somdiiser@gmail.com>
2023-01-23 18:02:30 +0000

    Different Way to compute slope

--- Codes1.1/Codes1D/ComputeTailDecayRate.m
+++ Codes1.1/Codes1D/ComputeTailDecayRate.m
@@ -3,13 +3,11 @@ function [dlogAbsF_dlogt] = ComputeTailDecayRate(T,F)
 logT   =log10(T);
 logAbsF=log10(abs(F));
 %
-DlogAbsF = logAbsF(2:end) - logAbsF(1:end-1);
-DlogT    = logT(2:end)    - logT(1:end-1);
+DlogAbsF = logAbsF(5:end) - logAbsF(1:end-4);
+DlogT    = logT(5:end)    - logT(1:end-4);
 
 % analytically the log time spacings should be this when T is equally space
 %DtTest = transpose((1+(1:length(T)-1))./ (1:length(T)-1));
 %logDtTest=log10(DtTest);
 %max(abs(DlogT-logDtTest));
-%DlogAbsF = reshape(DlogAbsF,length(DlogAbsF),1);
-%DlogT    = reshape(DlogT,length(DlogT),1);
 dlogAbsF_dlogt = DlogAbsF./DlogT;
